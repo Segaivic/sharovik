8381924cdb30260817fc1b37adb1abf41f90793c
Sega <sovremenny@live.ru>
2013-07-09 12:28:16 +0600

    поправил маршрут для ланча

--- protected/config/main.php
+++ protected/config/main.php
@@ -103,6 +103,7 @@ return array(
                 '/shop/admin'=>'shop/admin/index',
                 '/contacts' => 'site/contact',
                 '/login' => '/user/login',
+                'lunch/'=>'lunch/default',
                 'shop/category/<id:1>/<alias:.*?>'=>'shop/',
                 'shop/product/<id:\d+>/<alias:.*?>'=>'shop/product/view',
                 'shop/product/<alias_url:.*?>'=>'shop/product/view',

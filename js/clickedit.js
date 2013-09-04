f661c8005c4fd2ba25c2e93471a78b19e0a188fa
Sega <sovremenny@live.ru>
2013-09-04 17:47:08 +0600

    redactor fixed

--- js/clickedit.js
+++ js/clickedit.js
@@ -1,38 +1,38 @@
+var allowedTags = ["code", "span", "div", "label", "a", "br", "p", "b", "i", "del", "strike", "u",
+    "img", "video", "audio", "iframe", "object", "embed", "param", "blockquote",
+    "mark", "cite", "small", "ul", "ol", "li", "hr", "dl", "dt", "dd", "sup", "sub",
+    "big", "pre", "code", "figure", "figcaption", "strong", "em", "table", "tr", "td",
+    "th", "tbody", "thead", "tfoot", "h1", "h2", "h3", "h4", "h5", "h6", "center"];
+
 function exampleClickToEditPage()
 {
     $('.content').redactor({
-        imageUpload:'/admin/page/imageUpload/attr',
-        imageGetJson:'/admin/page/imageList/attr',
+        imageUpload:'/shop/admin/product/imageUpload/attr',
+        imageGetJson:'/shop/admin/product/imageList/attr',
         imageUploadErrorCallback:function(obj,json) { alert(json.error); },
         focus: true,
         convertDivs: false,
-        allowedTags: ["code", "span", "div", "label", "a", "br", "p", "b", "i", "del", "strike", "u",
-            "img", "video", "audio", "iframe", "object", "embed", "param", "blockquote",
-            "mark", "cite", "small", "ul", "ol", "li", "hr", "dl", "dt", "dd", "sup", "sub",
-            "big", "pre", "code", "figure", "figcaption", "strong", "em", "table", "tr", "td",
-            "th", "tbody", "thead", "tfoot", "h1", "h2", "h3", "h4", "h5", "h6", "center"],
+        allowedTags: allowedTags,
         lang: 'ru'});
 }
 
 function exampleClickToSavePage()
 {
     // save content if you need
-    var html = $('.content').getCode();
     var newsid = $('.content').attr('id').substr(7);
-
 
     $.ajax({
         type: 'post',
         dataType: 'json',
         data: {
             'id': newsid,
-            'html': html
+            'html': $('.content').redactor('get')
         },
         url: '/admin/news/savecontent'
     });
 
     // destroy editor
-    $('.content').destroyEditor();
+    $('.content').redactor('destroy');
 }
 
 function exampleClickToEditNews()
@@ -43,11 +43,7 @@ function exampleClickToEditNews()
         imageUploadErrorCallback:function(obj,json) { alert(json.error); },
         focus: true,
         convertDivs: false,
-        allowedTags: ["code", "span", "div", "label", "a", "br", "p", "b", "i", "del", "strike", "u",
-            "img", "video", "audio", "iframe", "object", "embed", "param", "blockquote",
-            "mark", "cite", "small", "ul", "ol", "li", "hr", "dl", "dt", "dd", "sup", "sub",
-            "big", "pre", "code", "figure", "figcaption", "strong", "em", "table", "tr", "td",
-            "th", "tbody", "thead", "tfoot", "h1", "h2", "h3", "h4", "h5", "h6", "center"],
+        allowedTags: allowedTags,
         lang: 'ru'});
 
     $('.fulltext').redactor({
@@ -56,19 +52,15 @@ function exampleClickToEditNews()
         imageUploadErrorCallback:function(obj,json) { alert(json.error); },
         focus: true,
         convertDivs: false,
-        allowedTags: ["code", "span", "div", "label", "a", "br", "p", "b", "i", "del", "strike", "u",
-            "img", "video", "audio", "iframe", "object", "embed", "param", "blockquote",
-            "mark", "cite", "small", "ul", "ol", "li", "hr", "dl", "dt", "dd", "sup", "sub",
-            "big", "pre", "code", "figure", "figcaption", "strong", "em", "table", "tr", "td",
-            "th", "tbody", "thead", "tfoot", "h1", "h2", "h3", "h4", "h5", "h6", "center"],
+        allowedTags: allowedTags,
         lang: 'ru'});
 }
 
 function exampleClickToSaveNews()
     {
         // save content if you need
-        var intro = $('.introtext').getCode();
-        var full = $('.fulltext').getCode();
+        var intro = $('.introtext').redactor('get');
+        var full = $('.fulltext').redactor('get');
         var newsid = $('.introtext').attr('id').substr(9);
 
 
@@ -84,8 +76,8 @@ function exampleClickToSaveNews()
         });
 
         // destroy editor
-        $('.introtext').destroyEditor();
-        $('.fulltext').destroyEditor();
+        $('.introtext').redactor('destroy');
+        $('.fulltext').redactor('destroy');
     }
 
 
@@ -97,18 +89,14 @@ function clickToEditProduct()
             imageUploadErrorCallback:function(obj,json) { alert(json.error); },
             focus: true,
             convertDivs: false,
-            allowedTags: ["code", "span", "div", "label", "a", "br", "p", "b", "i", "del", "strike", "u",
-                "img", "video", "audio", "iframe", "object", "embed", "param", "blockquote",
-                "mark", "cite", "small", "ul", "ol", "li", "hr", "dl", "dt", "dd", "sup", "sub",
-                "big", "pre", "code", "figure", "figcaption", "strong", "em", "table", "tr", "td",
-                "th", "tbody", "thead", "tfoot", "h1", "h2", "h3", "h4", "h5", "h6", "center"],
+            allowedTags: allowedTags,
             lang: 'ru'});
     }
 
 function clickToSaveProduct()
 {
     // save content if you need
-    var characters = $('.characters').getCode();
+    var characters = $('.characters').redactor('get');;
     var id = $('.characters').attr('id').substr(2);
 
 
@@ -123,5 +111,5 @@ function clickToSaveProduct()
     });
 
     // destroy editor
-    $('.characters').destroyEditor();
+    $('.characters').redactor('destroy');
 }
